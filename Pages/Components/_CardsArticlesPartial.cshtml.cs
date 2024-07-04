d58835aaec3ac0f64a22773cb2ed92a573e9b86d
Bassem Sherief <108496148+BassemAdam@users.noreply.github.com>
2024-07-04 08:15:52 +0300

    Improve responsiveness & truncating content on the homepage to avoid sending the entire content from the server to the client until the user views the full article.

--- Pages/Components/_CardsArticlesPartial.cshtml.cs
+++ Pages/Components/_CardsArticlesPartial.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 
@@ -63,6 +64,62 @@ namespace Wiki.Pages.Components
             Pages = pages; // Populate the Model.Pages property
 
             return Page();
+        }
+        public string TruncateContent(string content, int maxLength = 300)
+        {
+            if (string.IsNullOrEmpty(content)) return content;
+
+            // Check if the content is HTML
+            if (content.Contains("<"))
+            {
+                return TruncateHtml(content, maxLength);
+            }
+            else
+            {
+                return TruncatePlainText(content, maxLength);
+            }
+        }
+
+        private string TruncatePlainText(string content, int maxLength)
+        {
+            if (content.Length <= maxLength) return content;
+
+            return content.Substring(0, maxLength) + "...";
+        }
+
+        private string TruncateHtml(string html, int maxLength)
+        {
+            if (html.Length <= maxLength) return html;
+
+            // Truncate and close open tags
+            var truncatedHtml = html.Substring(0, maxLength) + "...";
+            return CloseOpenTags(truncatedHtml);
+        }
+
+        private string CloseOpenTags(string html)
+        {
+            var tags = new Stack<string>();
+            var regex = new Regex(@"<[^>]+>");
+
+            foreach (Match match in regex.Matches(html))
+            {
+                if (match.Value.StartsWith("</"))
+                {
+                    if (tags.Count > 0) tags.Pop();
+                }
+                else if (!match.Value.EndsWith("/>"))
+                {
+                    var tag = match.Value.Substring(1, match.Value.Length - 2).Split(' ')[0];
+                    tags.Push(tag);
+                }
+            }
+
+            while (tags.Count > 0)
+            {
+                html += $"</{tags.Pop()}>";
+            }
+
+            return html;
         }
     }
 }
--- Pages/Search.cshtml.cs
+++ Pages/Search.cshtml.cs
@@ -2,6 +2,7 @@
 using WikiPage = Wiki.Models.Page; // Alias for Wiki.Models.Page
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Wiki.Pages
 {
@@ -16,12 +17,75 @@ namespace Wiki.Pages
 
         public IEnumerable<WikiPage> SearchResults { get; private set; } // Use the alias here
 
-        public void OnGet(string query)
+       public void OnGet(string query)
         {
-            SearchResults = _wiki.GetAllPagesDetails()
-                .Where(p => p.Name.Contains(query, StringComparison.OrdinalIgnoreCase) ||
-                            p.Content.Contains(query, StringComparison.OrdinalIgnoreCase))
-                .ToList();
+            if (string.IsNullOrEmpty(query))
+            {
+                SearchResults = _wiki.GetAllPagesDetails().ToList();
+            }
+            else
+            {
+                SearchResults = _wiki.GetAllPagesDetails()
+                    .Where(p => p.Name.Contains(query, StringComparison.OrdinalIgnoreCase))
+                    .ToList();
+            }
+        }
+    
+    public string TruncateContent(string content, int maxLength = 300)
+    {
+        if (string.IsNullOrEmpty(content)) return content;
+
+        // Check if the content is HTML
+        if (content.Contains("<"))
+        {
+            return TruncateHtml(content, maxLength);
+        }
+        else
+        {
+            return TruncatePlainText(content, maxLength);
         }
     }
+
+    private string TruncatePlainText(string content, int maxLength)
+    {
+        if (content.Length <= maxLength) return content;
+
+        return content.Substring(0, maxLength) + "...";
+    }
+
+    private string TruncateHtml(string html, int maxLength)
+    {
+        if (html.Length <= maxLength) return html;
+
+        // Truncate and close open tags
+        var truncatedHtml = html.Substring(0, maxLength) + "...";
+        return CloseOpenTags(truncatedHtml);
+    }
+
+    private string CloseOpenTags(string html)
+    {
+        var tags = new Stack<string>();
+        var regex = new Regex(@"<[^>]+>");
+
+        foreach (Match match in regex.Matches(html))
+        {
+            if (match.Value.StartsWith("</"))
+            {
+                if (tags.Count > 0) tags.Pop();
+            }
+            else if (!match.Value.EndsWith("/>"))
+            {
+                var tag = match.Value.Substring(1, match.Value.Length - 2).Split(' ')[0];
+                tags.Push(tag);
+            }
+        }
+
+        while (tags.Count > 0)
+        {
+            html += $"</{tags.Pop()}>";
+        }
+
+        return html;
+    }
+}
 }
